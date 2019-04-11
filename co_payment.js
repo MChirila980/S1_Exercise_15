af5aa83aa2542906fb0ec292b4ba0571950eeddf
Melinda Chirila <mchirila980@west-mec.org>
2019-04-11 14:37:50 -0700

    Commit 4/11/2019

--- co_payment.js
+++ co_payment.js
@@ -44,3 +44,71 @@
 
 */
 
+//displays the current date when it is loaded by the browser
+window.addEventListener("load", function() { 
+      var orderForm = document.forms.orderForm; 
+      orderForm.elements.orderDate.value = new Date().toDateString();
+      orderForm.elements.model.focus();
+      });
+
+      //for the Calcorder function
+      function calcOrder() { var orderForm = document.forms.orderForm;
+
+      // Calculate the initial cost of the order 
+      var mIndex = orderForm.elements.model.selectedIndex; 
+      var mCost = orderForm.elements.model.options[mIndex].value; 
+      var qIndex = orderForm.elements.qty.selectedIndex; 
+      var quantity = orderForm.elements.qty[qIndex].value;
+
+      // Initial cost = cost x quantity 
+      var initialCost = mCost*quantity; 
+      orderForm.elements.initialCost.value = initialCost;
+
+      orderForm.elements.initialCost.value = formatUSCurrency(initialCost);
+
+      orderForm.elements.protectionCost.value = formatNumber(pCost, 2);
+
+      orderForm.elements.subtotal.value = formatNumber(initialCost + pCost, 2);
+
+      orderForm.elements.salesTax.value = formatNumber(salesTax, 2);
+
+      orderForm.elements.totalCost.value = formatUSCurrency(totalCost);
+
+      // Calculate the cost of the order 
+      calcOrder();
+
+      // Retrieve the cost of the user's protection plan 
+      var pCost = document.querySelector('input[name=”protection”]: checked').value*quantity; 
+      orderForm.elements.protectionCost.value = pCost;
+
+      // Calculate the order subtotal 
+      orderForm.elements.subtotal.value = initialCost + pCost;
+
+      // Calculate the sales tax 
+      var salesTax = 0.05*(initialCost + pCost); 
+      orderForm.elements.salesTax.value = salesTax;
+
+      // Calculate the cost of the total order 
+      var totalCost = initialCost + pCost + salesTax; 
+      orderForm.elements.totalCost.value = totalCost;
+
+      }
+      
+      function formatNumber(val, decimals) { 
+            return val.toLocaleString(undefined,
+                  {minimumFractionDigits: decimals, maximumFractionDigits: decimals});
+            }
+            
+            function formatUSCurrency(val) { 
+            return val.toLocaleString('en-US',
+            {style: "currency", currency: "USD"} );
+
+            
+            
+   
+            }
+            
+
+
+
+
